d784f528a40b87a98909530ab27b7ce6a1b8f9dc
GusGF <21323227+GusGF@users.noreply.github.com>
2024-01-23 19:07:01 +0000

    Got basic equations working

--- calculator.js
+++ calculator.js
@@ -1,15 +1,15 @@
-let inputArr;
-inputArr = [];
+let inputArrStr_g;
+inputArrStr_g = [];
 
 // Event listener picks up all key presses till it receives a '='
 // then the expression which is stored in an array is evaluated
 document.addEventListener("click", (event) => {
-  inputArr[inputArr.length] = event.target.getAttribute("data-key");
-  console.log(`The array length is: ${inputArr.length}`);
+  inputArrStr_g[inputArrStr_g.length] = event.target.getAttribute("data-key");
+  console.log(`The array length is: ${inputArrStr_g.length}`);
   // console.log(event.target.getAttribute("data-key"));
-  if (inputArr[inputArr.length - 1] == "=") {
-    console.log(inputArr);
-    evaluate();
+  if (inputArrStr_g[inputArrStr_g.length - 1] == "=") {
+    console.log(`Raw input: ${inputArrStr_g}`);
+    createArrayEq();
   }
 });
 
@@ -34,10 +34,62 @@ function display(answer) {
   console.log(answer);
 }
 
-function evaluate() {
-  let operandOne = null,
-    operandTwo = null,
-    operator = null;
-  let genOperand = "";
-  inputArr.forEach((element) => {});
+function createArrayEq() {
+  let operator;
+  let operand = "";
+  let inputArrValues_v = [];
+  console.log(`This should be empty: ${inputArrValues_v}`);
+  inputArrStr_g.forEach((element) => {
+    // is this a valid numeric value or a decimal point?
+    if (!isNaN(parseFloat(element)) || element == ".") {
+      operand = operand.concat(element);
+    }
+    // an operator has been found so save operator and above operand
+    else {
+      operator = element;
+      inputArrValues_v[inputArrValues_v.length] = operand;
+      inputArrValues_v[inputArrValues_v.length] = operator;
+      // reset operand
+      operand = "";
+    }
+  });
+  // reset global raw input array
+  inputArrStr_g = [];
+  console.log(`Equation: ${inputArrValues_v}`);
+  evaluate(inputArrValues_v);
+}
+
+function evaluate(p_inputArrV) {
+  let valueOne = 0;
+  let valueTwo = 0;
+  let operator = "";
+  let tally = 0;
+  console.log("Hello");
+  for (let i = 0; i < p_inputArrV.length - 1; i++) {
+    // Get sub-equations
+    // if (p_inputArrV[i] == "=") {
+    //   break;
+    // }
+    if (valueOne == 0) {
+      valueOne = p_inputArrV[i];
+    } else if (isNaN(p_inputArrV[i])) {
+      operator = p_inputArrV[i];
+    } else {
+      valueTwo = p_inputArrV[i];
+    }
+    // When sub-equation is extracted fully, solve
+    if (valueOne && operator && valueTwo) {
+      let answer = solveEq(valueOne, valueTwo, operator);
+      // Have we reached the end of the equation? If not keep going and
+      // assign the result of the previous calculation to valueOne
+      if (p_inputArrV[i + 1] != "=") {
+        valueOne = answer;
+      } else {
+        tally = answer;
+        break;
+      }
+    }
+  }
+  console.log(`Answer is: ${tally}`);
+  display(tally);
 }
